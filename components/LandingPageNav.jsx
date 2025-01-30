a584eaca230152708a8cd252f8382d7965a6692d
YoYoDough <yosuf818s@gmail.com>
2025-01-30 15:49:52 -0800

    adding links to nav

--- components/LandingPageNav.jsx
+++ components/LandingPageNav.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 const LandingPageNav = () => {
   return (
-    <nav className = "flex w-4/5">
+    <nav className = "flex w-4/5 items-center justify-between">
         <div>
             <img className = "w-40" src = "logo.png"></img>
         </div>
 
-        <div>
-            
+        <div className = "flex">
+            <Link href = "/feature" className = "font-clash p-2">Feature</Link>
+            <Link href = '/about' className = "font-clash p-2">About</Link>
+            <Link href = '/blog' className = "font-clash p-2">Blog</Link>
         </div>
         
+        <div>
+            <Link href = "/signup">Sign Up</Link>
+            <Link href = '/login'>Log In</Link>
+        </div>
     </nav>
   )
 }
