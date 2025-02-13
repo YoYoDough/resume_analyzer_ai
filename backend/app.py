d5ec78949887709b4cb407e7997fd439c4b165cb
YoYoDough <yosuf818s@gmail.com>
2025-02-13 15:57:25 -0800

    file name and virtual environment are set

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,20 @@
 ##Going to be the place where Resume is analyzed...
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os, openai
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/upload', methods = ["POST"])
 def upload_file():
+    file = request.files['file']
+    
     return jsonify({
         "message": "File uploaded and analyzed successfully",
         "filename": file.filename,
         "analysis": analysis,
     })
+
+if __name__ == "__main__":
+    app.run(debug=True)
